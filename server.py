b7574626b7f36c2e5e60e97050970936c76062c7
Christopher Thong <christhong123@gmail.com>
2020-01-17 15:16:28 +0800

    Created an instance to save a store in the DB. Refer to lines 4, 24, 25 of server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import peeweedbevolve # For migration
 from flask import Flask, render_template, request
 from models import db
+from models import Store
 
 app = Flask(__name__)
 
@@ -19,12 +20,14 @@ def migrate(): # new
 
 @app.route("/")
 def index():
-   return render_template('index.html')
+   store_name = request.args.get('store_name')
+   save_store_name = Store(name=store_name) # Create an instance (Store is the table in models.py)
+   save_store_name.save() 
+   return render_template('index.html',store_name=store_name)
 
 @app.route("/store")   
 def store():
-   store_name = request.args.get('store_name')
-   return render_template('store.html',store_name=store_name)   
+   return render_template('store.html')   
 
 if __name__ == '__main__':
    app.run()
