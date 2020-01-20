601314d10b6c15469b6920e15e675a2ff1925834
Christopher Thong <christhong123@gmail.com>
2020-01-20 16:18:00 +0800

    Created a page to view all stores and number of stores, added delete buttons to delete stores, added a page to edit store names

--- server.py
+++ server.py
@@ -23,10 +23,6 @@ def migrate(): # new
 
 @app.route("/")
 def index():
-   # store_name = request.args.get('store_name')
-   # save_store_name = Store(name=store_name) # Create an instance (Store is the table in models.py)
-   # save_store_name.save() 
-   # return render_template('index.html',store_name=store_name)
    return render_template('index.html')
 
 @app.route("/store")   
@@ -36,8 +32,6 @@ def store():
 @app.route("/store_form", methods=["POST"])
 def store_form():
    store_name = Store(name=request.form.get('store_name'))
-   # store_name = request.args.get('store_name')
-   # save_store_name = Store(name=store_name) # Create an instance (Store is the table in models.py)
    if store_name.save():
       flash("Store successfully saved")
       return redirect(url_for('store'))
@@ -61,7 +55,33 @@ def warehouse_form():
    else:   
       return render_template('warehouse.html',warehouse_location=warehouse_location, warehouse_store=warehouse_store)   
 
-   
+@app.route("/store/<id>")
+def store_info(id):
+   store_obj = Store.get_by_id(id)
+   count_no_warehouse = len([wh.location for wh in store_obj.warehouses])
+   return render_template('editstore.html', store_obj = store_obj, count_no_warehouse=count_no_warehouse)
+
+@app.route("/store_info_edit/<id>", methods=["POST"])
+def store_info_edit(id):
+   store_obj = Store.get_by_id(id) # gets the row in Store table that matches the id
+   store_name = request.form.get('store_name') # getting value from editstore.html
+   store_obj.name = store_name # Setting a new name for the name in the row of Store table that matches the id 
+   if store_obj.save():
+      return redirect(url_for('store_info', id=id))  
+
+@app.route("/store/content")
+def content():
+   stores = Store.select() # Selects the whole table
+   return render_template('content.html', stores=stores)
+
+@app.route("/store/delete", methods=["POST"])
+def delete():
+   stores = Store.select() # Selects the whole table
+   id_to_delete = request.form.get('delete-btn') # Set the variable, id_to_delete, to the value of the button named, delete-btn
+   instance_to_delete = Store.get_by_id(id_to_delete) # Select the row where id_to_delete is
+   Warehouse.delete().where(Warehouse.store_id == id_to_delete).execute() # You need to delete from Warehouse before you delete from Store
+   instance_to_delete.delete_instance()
+   return redirect(url_for('content'))   
 
 if __name__ == '__main__':
    app.run()
